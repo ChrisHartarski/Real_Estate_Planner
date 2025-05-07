af3d459808075e6880df59d7d8fc82003d174215
ChrisHartarski <c.hartarski@gmail.com>
2025-05-07 10:32:01 +0300

    Initial commit

---
+++ src/main/java/com/butikimoti/real_estate_planner/RealEstatePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.butikimoti.real_estate_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstatePlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstatePlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/butikimoti/real_estate_planner/RealEstatePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.butikimoti.real_estate_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstatePlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
