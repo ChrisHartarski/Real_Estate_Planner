2c06cce31d244d0b014b03d83f2ea504cf69de92
ChrisHartarski <c.hartarski@gmail.com>
2025-05-19 15:28:03 +0300

    Created the @PostMapping register for UserController.

--- src/main/java/com/butikimoti/real_estate_planner/controller/UserController.java
+++ src/main/java/com/butikimoti/real_estate_planner/controller/UserController.java
@@ -1,28 +1,28 @@
 package com.butikimoti.real_estate_planner.controller;
 
 import com.butikimoti.real_estate_planner.model.dto.userEntity.RegisterUserDTO;
-import com.butikimoti.real_estate_planner.model.entity.UserEntity;
 import com.butikimoti.real_estate_planner.service.CompanyService;
 import com.butikimoti.real_estate_planner.service.UserEntityService;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
     private final UserEntityService userEntityService;
     private final CompanyService companyService;
-    private final ModelMapper modelMapper;
 
     public UserController(UserEntityService userEntityService, CompanyService companyService, ModelMapper modelMapper) {
         this.userEntityService = userEntityService;
         this.companyService = companyService;
-        this.modelMapper = modelMapper;
     }
 
     @ModelAttribute("registerUserData")
@@ -36,9 +36,35 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String register(UserEntity userEntity) {
-        //TODO:
+    public String register(@Valid RegisterUserDTO registerUserData,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes) {
 
+        if(bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("registerUserData", registerUserData);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.registerUserData", bindingResult);
+            return "redirect:/users/register";
+        }
+
+        if (userEntityService.userExists(registerUserData.getEmail())) {
+            redirectAttributes.addFlashAttribute("registerUserData", registerUserData);
+            redirectAttributes.addFlashAttribute("userExists", true);
+            return "redirect:/users/register";
+        }
+
+        if (!registerUserData.getPassword().equals(registerUserData.getConfirmPassword())) {
+            redirectAttributes.addFlashAttribute("registerUserData", registerUserData);
+            redirectAttributes.addFlashAttribute("passwordsDoNotMatch", true);
+            return "redirect:/users/register";
+        }
+
+        if (!companyService.companyExists(registerUserData.getCompanyName())) {
+            redirectAttributes.addFlashAttribute("registerUserData", registerUserData);
+            redirectAttributes.addFlashAttribute("companyDoesNotExist", true);
+            return "redirect:/users/register";
+        }
+
+        userEntityService.register(registerUserData);
         return "redirect:/users/login";
     }
 
@@ -52,6 +78,5 @@ public class UserController {
         model.addAttribute("wrongUsernameOrPassword", true);
         return "login";
     }
-
 
 }
--- src/main/java/com/butikimoti/real_estate_planner/service/impl/UserEntityServiceImpl.java
+++ src/main/java/com/butikimoti/real_estate_planner/service/impl/UserEntityServiceImpl.java
@@ -1,19 +1,30 @@
 package com.butikimoti.real_estate_planner.service.impl;
 
+import com.butikimoti.real_estate_planner.model.dto.userEntity.RegisterUserDTO;
+import com.butikimoti.real_estate_planner.model.entity.UserEntity;
 import com.butikimoti.real_estate_planner.repository.UserEntityRepository;
 import com.butikimoti.real_estate_planner.service.UserEntityService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserEntityServiceImpl implements UserEntityService {
     private final UserEntityRepository userEntityRepository;
+    private final ModelMapper modelMapper;
 
-    public UserEntityServiceImpl(UserEntityRepository userEntityRepository) {
+    public UserEntityServiceImpl(UserEntityRepository userEntityRepository, ModelMapper modelMapper) {
         this.userEntityRepository = userEntityRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     public boolean userExists(String email) {
         return userEntityRepository.existsByEmail(email);
+    }
+
+    @Override
+    public void register(RegisterUserDTO registerUserDTO) {
+        UserEntity user = modelMapper.map(registerUserDTO, UserEntity.class);
+        userEntityRepository.saveAndFlush(user);
     }
 }
