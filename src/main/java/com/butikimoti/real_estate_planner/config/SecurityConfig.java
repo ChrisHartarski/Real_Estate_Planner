b68e535079f24b42cc56fbb8c458e2bd7e0c6e5b
ChrisHartarski <c.hartarski@gmail.com>
2025-05-21 10:41:36 +0300

    Added security filter chain.

--- src/main/java/com/butikimoti/real_estate_planner/config/SecurityConfig.java
+++ src/main/java/com/butikimoti/real_estate_planner/config/SecurityConfig.java
@@ -1,12 +1,50 @@
 package com.butikimoti.real_estate_planner.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                //grant access to static resources
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                //access to all users
+                                .requestMatchers("/", "/users/login", "/users/register", "/companies/register").permitAll()
+                                //access to ADMIN role users
+                                .requestMatchers("/admin-panel").hasRole("ADMIN")
+                                //access to COMPANY_ADMIN role users
+                                .requestMatchers("/companies/edit").hasRole("COMPANY_ADMIN")
+                                //access to USER role users
+                                .requestMatchers("/sales", "/sales/property-page").hasRole("USER")
+                                .anyRequest()
+                                .authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/users/login")
+                                .usernameParameter("email")
+                                .passwordParameter("password")
+                                .defaultSuccessUrl("/", true)
+                                .failureUrl("/users/login-error")
+                )
+                .logout(logout ->
+                        logout
+                                .logoutUrl("/users/logout")
+                                .logoutSuccessUrl("/")
+                                .invalidateHttpSession(true)
+                )
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
