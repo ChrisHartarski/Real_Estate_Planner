a17a2d3a0ab626122846d5b5cb0409c87cf77084
ChrisHartarski <c.hartarski@gmail.com>
2025-05-22 16:17:01 +0300

    Renamed the Sales controller and template to properties. Will make the same templates work both for rent and sale offers with parameter offerType distinguishing between the two.

--- src/main/java/com/butikimoti/real_estate_planner/controller/PropertiesController.java
+++ src/main/java/com/butikimoti/real_estate_planner/controller/PropertiesController.java
@@ -1,7 +1,6 @@
 package com.butikimoti.real_estate_planner.controller;
 
 import com.butikimoti.real_estate_planner.model.dto.property.PropertyDTO;
-import com.butikimoti.real_estate_planner.model.entity.BaseProperty;
 import com.butikimoti.real_estate_planner.model.enums.SaleOrRent;
 import com.butikimoti.real_estate_planner.service.BasePropertyService;
 import org.springframework.data.domain.Pageable;
@@ -11,30 +10,41 @@ import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
-public class SalesController {
+public class PropertiesController {
     private final BasePropertyService basePropertyService;
 
-    public SalesController(BasePropertyService basePropertyService) {
+    public PropertiesController(BasePropertyService basePropertyService) {
         this.basePropertyService = basePropertyService;
     }
 
-
-    @GetMapping("/sales")
-    public String viewSales(@PageableDefault(size = 10, sort = "updatedOn", direction = Sort.Direction.DESC) Pageable pageable,
+    @GetMapping("/properties")
+    public String viewSales(@RequestParam(value = "offerType") String offerType,
+                            @PageableDefault(size = 10, sort = "updatedOn", direction = Sort.Direction.DESC) Pageable pageable,
                             Model model) {
-        PagedModel<PropertyDTO> properties = basePropertyService.getAllPropertiesByCompany(pageable, SaleOrRent.SALE);
+        SaleOrRent saleOrRent = mapSaleOrRent(offerType);
+        PagedModel<PropertyDTO> properties = basePropertyService.getAllPropertiesByCompany(pageable, saleOrRent);
         model.addAttribute("properties", properties);
-        return "sales";
+        return "properties";
     }
 
     @GetMapping("/sales/property-page")
     public String getOfferPage() {
         return "property-page";
+    }
+
+    private SaleOrRent mapSaleOrRent (String offerType) {
+        if (offerType.equalsIgnoreCase("sale")) {
+            return SaleOrRent.SALE;
+        }
+
+        if (offerType.equalsIgnoreCase("rent")) {
+            return SaleOrRent.RENT;
+        }
+
+        throw new IllegalArgumentException("Invalid saleOrRent: " + offerType);
     }
 }
