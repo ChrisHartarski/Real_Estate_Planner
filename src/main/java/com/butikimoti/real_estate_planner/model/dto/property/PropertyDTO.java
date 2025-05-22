659e8c35361ed57735a6be22dc105018ff627396
ChrisHartarski <c.hartarski@gmail.com>
2025-05-22 15:35:59 +0300

    Created PropertyDTO with fields for Apartment and House.

---
+++ src/main/java/com/butikimoti/real_estate_planner/model/dto/property/PropertyDTO.java
@@ -0,0 +1,263 @@
+package com.butikimoti.real_estate_planner.model.dto.property;
+
+import com.butikimoti.real_estate_planner.model.enums.*;
+
+import java.time.LocalDateTime;
+
+public class PropertyDTO {
+    private PropertyType propertyType;
+
+    private String ownerCompanyName;
+
+    private String address;
+
+    private double price;
+
+    private int area;
+
+    private AreaUnit areaUnit;
+
+    private SaleOrRent saleOrRent;
+
+    private String contactName;
+
+    private String contactPhone;
+
+    private String contactEmail;
+
+    private String description;
+
+    private LocalDateTime createdOn;
+
+    private LocalDateTime updatedOn;
+
+    //common
+    private ConstructionType constructionType;
+
+    private int year;
+
+    private int roomCount;
+
+    private int floor;
+
+    private int buildingFloors;
+
+    private String facing;
+
+    //apartment
+    private ApartmentType apartmentType;
+
+    private boolean hasElevator;
+
+    //house
+    private HouseType houseType;
+
+    private int yardArea;
+
+    private int floors;
+
+    private String additionalStructures;
+
+    public PropertyDTO() {
+    }
+
+    public PropertyType getPropertyType() {
+        return propertyType;
+    }
+
+    public void setPropertyType(PropertyType propertyType) {
+        this.propertyType = propertyType;
+    }
+
+    public String getOwnerCompanyName() {
+        return ownerCompanyName;
+    }
+
+    public void setOwnerCompanyName(String ownerCompanyName) {
+        this.ownerCompanyName = ownerCompanyName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public AreaUnit getAreaUnit() {
+        return areaUnit;
+    }
+
+    public void setAreaUnit(AreaUnit areaUnit) {
+        this.areaUnit = areaUnit;
+    }
+
+    public SaleOrRent getSaleOrRent() {
+        return saleOrRent;
+    }
+
+    public void setSaleOrRent(SaleOrRent saleOrRent) {
+        this.saleOrRent = saleOrRent;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public LocalDateTime getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(LocalDateTime updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public ConstructionType getConstructionType() {
+        return constructionType;
+    }
+
+    public void setConstructionType(ConstructionType constructionType) {
+        this.constructionType = constructionType;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getRoomCount() {
+        return roomCount;
+    }
+
+    public void setRoomCount(int roomCount) {
+        this.roomCount = roomCount;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getBuildingFloors() {
+        return buildingFloors;
+    }
+
+    public void setBuildingFloors(int buildingFloors) {
+        this.buildingFloors = buildingFloors;
+    }
+
+    public String getFacing() {
+        return facing;
+    }
+
+    public void setFacing(String facing) {
+        this.facing = facing;
+    }
+
+    public ApartmentType getApartmentType() {
+        return apartmentType;
+    }
+
+    public void setApartmentType(ApartmentType apartmentType) {
+        this.apartmentType = apartmentType;
+    }
+
+    public boolean isHasElevator() {
+        return hasElevator;
+    }
+
+    public void setHasElevator(boolean hasElevator) {
+        this.hasElevator = hasElevator;
+    }
+
+    public HouseType getHouseType() {
+        return houseType;
+    }
+
+    public void setHouseType(HouseType houseType) {
+        this.houseType = houseType;
+    }
+
+    public int getYardArea() {
+        return yardArea;
+    }
+
+    public void setYardArea(int yardArea) {
+        this.yardArea = yardArea;
+    }
+
+    public int getFloors() {
+        return floors;
+    }
+
+    public void setFloors(int floors) {
+        this.floors = floors;
+    }
+
+    public String getAdditionalStructures() {
+        return additionalStructures;
+    }
+
+    public void setAdditionalStructures(String additionalStructures) {
+        this.additionalStructures = additionalStructures;
+    }
+}
---
+++ src/main/java/com/butikimoti/real_estate_planner/model/enums/PropertyType.java
@@ -0,0 +1,9 @@
+package com.butikimoti.real_estate_planner.model.enums;
+
+public enum PropertyType {
+    APARTMENT,
+    BUSINESS_PROPERTY,
+    GARAGE,
+    HOUSE,
+    LAND
+}
