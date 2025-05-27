8f32be4583dce52e307b654807ac345ee0a30647
ChrisHartarski <c.hartarski@gmail.com>
2025-05-27 17:27:07 +0300

    Made functionality to show different fields in the add-property.html depending on propertyType field.

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const propertyTypeSelect = document.getElementById("propertyType");
+
+    function toggleFields(selectedTypeValue) {
+        const allPropertyTypes = ['apartment', 'house', 'business', 'garage', 'land'];
+        const selectedType = selectedTypeValue.toLowerCase();
+
+        // Hide all fields
+        document.querySelectorAll('.property-type-main')
+            .forEach(el => el.classList.add('hidden'));
+
+        allPropertyTypes.forEach(type => {
+            document.querySelectorAll(`.property-type-${type}`)
+                .forEach(el => el.classList.add('hidden'))
+        });
+
+        // Show main and selected fields
+        if (allPropertyTypes.includes(selectedType)) {
+            document.querySelectorAll(`.property-type-main, .property-type-${selectedType}`)
+                .forEach(el => el.classList.remove('hidden'));
+        }
+    }
+
+    propertyTypeSelect
+        .addEventListener("change", () => toggleFields(propertyTypeSelect.value));
+})
