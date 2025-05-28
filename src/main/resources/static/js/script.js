c4cefd4ada4bb87d0f85e6b07f379573a2aae4ab
ChrisHartarski <c.hartarski@gmail.com>
2025-05-28 16:55:35 +0300

    Edited the script to toggleFields when DOM loads so that when there is reload due to error the correct fields are shown.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,26 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     const propertyTypeSelect = document.getElementById("propertyType");
-
-    function toggleFields(selectedTypeValue) {
-        const allPropertyTypes = ['apartment', 'house', 'business', 'garage', 'land'];
-        const selectedType = selectedTypeValue.toLowerCase();
-
-        // Hide all fields
-        document.querySelectorAll('.property-type-main')
-            .forEach(el => el.classList.add('hidden'));
-
-        allPropertyTypes.forEach(type => {
-            document.querySelectorAll(`.property-type-${type}`)
-                .forEach(el => el.classList.add('hidden'))
-        });
-
-        // Show main and selected fields
-        if (allPropertyTypes.includes(selectedType)) {
-            document.querySelectorAll(`.property-type-main, .property-type-${selectedType}`)
-                .forEach(el => el.classList.remove('hidden'));
-        }
-    }
+    toggleFields(propertyTypeSelect.value);
 
     propertyTypeSelect
         .addEventListener("change", () => toggleFields(propertyTypeSelect.value));
-})
+});
+
+function toggleFields(selectedTypeValue) {
+    const allPropertyTypes = ['apartment', 'house', 'business', 'garage', 'land'];
+    const selectedType = selectedTypeValue.toLowerCase();
+
+    // Hide all fields
+    document.querySelectorAll('.property-type-main')
+        .forEach(el => el.classList.add('hidden'));
+
+    allPropertyTypes.forEach(type => {
+        document.querySelectorAll(`.property-type-${type}`)
+            .forEach(el => el.classList.add('hidden'))
+    });
+
+    // Show main and selected fields
+    if (allPropertyTypes.includes(selectedType)) {
+        document.querySelectorAll(`.property-type-main, .property-type-${selectedType}`)
+            .forEach(el => el.classList.remove('hidden'));
+    }
+}
