a4c4a882c010613ffb6a4e6fac99d49310572aca
ChrisHartarski <c.hartarski@gmail.com>
2025-06-05 12:41:04 +0300

    Added functionality to upload picture to property. TODO: make the picture visuals better.

--- src/main/java/com/butikimoti/real_estate_planner/controller/PropertyController.java
+++ src/main/java/com/butikimoti/real_estate_planner/controller/PropertyController.java
@@ -3,9 +3,11 @@ package com.butikimoti.real_estate_planner.controller;
 import com.butikimoti.real_estate_planner.model.dto.property.AddPropertyDTO;
 import com.butikimoti.real_estate_planner.model.dto.property.PropertyDTO;
 import com.butikimoti.real_estate_planner.model.entity.BaseProperty;
+import com.butikimoti.real_estate_planner.model.entity.PropertyPicture;
 import com.butikimoti.real_estate_planner.model.enums.OfferType;
 import com.butikimoti.real_estate_planner.service.BasePropertyService;
 import com.butikimoti.real_estate_planner.service.UserEntityService;
+import com.butikimoti.real_estate_planner.service.util.CloudinaryService;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Pageable;
@@ -16,8 +18,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.Properties;
 import java.util.UUID;
 
@@ -26,11 +30,13 @@ import java.util.UUID;
 public class PropertyController {
     private final BasePropertyService basePropertyService;
     private final UserEntityService userEntityService;
+    private final CloudinaryService cloudinaryService;
     private final ModelMapper modelMapper;
 
-    public PropertyController(BasePropertyService basePropertyService, UserEntityService userEntityService, ModelMapper modelMapper) {
+    public PropertyController(BasePropertyService basePropertyService, UserEntityService userEntityService, CloudinaryService cloudinaryService, ModelMapper modelMapper) {
         this.basePropertyService = basePropertyService;
         this.userEntityService = userEntityService;
+        this.cloudinaryService = cloudinaryService;
         this.modelMapper = modelMapper;
     }
 
@@ -96,6 +102,20 @@ public class PropertyController {
 
         model.addAttribute("property", propertyData);
         return "property-page";
+    }
+
+    @PostMapping("/{id}/upload-picture")
+    public String uploadPicture(@PathVariable UUID id,
+                                @RequestParam("image") MultipartFile file) throws IOException {
+        BaseProperty property = basePropertyService.getPropertyByID(id);
+        String imageUrl = cloudinaryService.uploadImage(file);
+
+        PropertyPicture picture = new PropertyPicture();
+        picture.setPictureLink(imageUrl);
+        property.getPictures().add(picture);
+        basePropertyService.updateProperty(property);
+
+        return "redirect:/properties/" + id;
     }
 
     private PropertyDTO getPropertyDTOById(UUID id) {
