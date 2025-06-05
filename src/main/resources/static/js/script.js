687785b1acfe1501d21ae33f072910f5b587e35e
ChrisHartarski <c.hartarski@gmail.com>
2025-06-05 15:59:34 +0300

    Changed the script.js (added if statements so the script can work for multiple pages)

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM loaded");
     const propertyTypeSelect = document.getElementById("propertyType");
-    toggleFields(propertyTypeSelect.value);
 
-    propertyTypeSelect
-        .addEventListener("change", () => toggleFields(propertyTypeSelect.value));
+    if(propertyTypeSelect) {
+        toggleFields(propertyTypeSelect.value);
+        propertyTypeSelect
+            .addEventListener("change", () => toggleFields(propertyTypeSelect.value));
+    }
+
+    const input = document.getElementById('file-upload');
+    const fileName = document.getElementById('file-name');
+
+    if(input && fileName) {
+        input.addEventListener('change', () => {
+            if(input.files.length > 0){
+                fileName.textContent = input.files[0].name;
+            }
+        });
+    }
 });
 
 function toggleFields(selectedTypeValue) {
