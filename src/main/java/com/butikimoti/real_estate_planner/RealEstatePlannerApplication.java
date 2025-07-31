49db7affe8de111e3e243af7b5b3ea17b8326c7d
ChrisHartarski <c.hartarski@gmail.com>
2025-07-31 10:08:54 +0300

    Changed the dotenv dependency so it loads the variables.env file (had to also add it to the RealEstatePlannerApplication.java)

--- src/main/java/com/butikimoti/real_estate_planner/RealEstatePlannerApplication.java
+++ src/main/java/com/butikimoti/real_estate_planner/RealEstatePlannerApplication.java
@@ -1,5 +1,6 @@
 package com.butikimoti.real_estate_planner;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RealEstatePlannerApplication {
 
     public static void main(String[] args) {
+        // load .env file
+        Dotenv dotenv = Dotenv.configure()
+                .filename("variables.env")
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        // run
         SpringApplication.run(RealEstatePlannerApplication.class, args);
     }
 }
