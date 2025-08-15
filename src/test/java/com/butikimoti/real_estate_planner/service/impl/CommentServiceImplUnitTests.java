d65d5694f49a1d0f07d28cbcc6bacf562093834d
ChrisHartarski <c.hartarski@gmail.com>
2025-08-15 14:16:05 +0300

    Fix tests so it is ADMIN user registering companies.

--- src/test/java/com/butikimoti/real_estate_planner/service/impl/CommentServiceImplUnitTests.java
+++ src/test/java/com/butikimoti/real_estate_planner/service/impl/CommentServiceImplUnitTests.java
@@ -32,7 +32,7 @@ public class CommentServiceImplUnitTests {
     private static final Company TEST_COMPANY = new Company(TEST_COMPANY_ID, new ArrayList<>(), "Test Company", "Test Address", "+359000000000", "test@email.com", new ArrayList<>());
     private static final UserEntity TEST_USER = new UserEntity(TEST_USER_ID, "user@mail.com", TEST_COMPANY, "password", UserRole.COMPANY_ADMIN, "name", "last_name", "+359 000 000 000");
     private static final Apartment TEST_APARTMENT = new Apartment(TEST_APARTMENT_ID, PropertyType.APARTMENT, TEST_COMPANY, "test_city", "test_neighbourhood", "test_address", 70000.00, 70, AreaUnit.SQUARE_METER, OfferType.SALE, "contact_name", "+359 000 000 000", "contact@mail.com", "description", LocalDateTime.now(), LocalDateTime.now(), ApartmentType.THREE_ROOM, 3, ConstructionType.BRICK, 2000, 3, 8, HeatingType.GAS, true, "facing");
-    private static final AddCommentDTO TEST_ADD_COMMENT_DTO = new AddCommentDTO("comment_text", TEST_USER, TEST_APARTMENT);
+    private static final AddCommentDTO TEST_ADD_COMMENT_DTO = new AddCommentDTO("comment_text", TEST_USER.getFirstName(), TEST_USER.getLastName(), TEST_APARTMENT);
 
 
     @Mock
@@ -56,7 +56,8 @@ public class CommentServiceImplUnitTests {
         Assertions.assertNotNull(actual);
         Assertions.assertEquals(TEST_ADD_COMMENT_DTO.getCommentText(), actual.getCommentText());
         Assertions.assertEquals(TEST_ADD_COMMENT_DTO.getProperty(), actual.getProperty());
-        Assertions.assertEquals(TEST_ADD_COMMENT_DTO.getUser(), actual.getUser());
+        Assertions.assertEquals(TEST_ADD_COMMENT_DTO.getUserFirstName(), actual.getUserFirstName());
+        Assertions.assertEquals(TEST_ADD_COMMENT_DTO.getUserLastName(), actual.getUserLastName());
         Assertions.assertNotNull(actual.getDate());
     }
 }
