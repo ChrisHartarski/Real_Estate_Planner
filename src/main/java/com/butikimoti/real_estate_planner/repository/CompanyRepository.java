be943224b44fd810600400f0569fda5a503a2f6b
ChrisHartarski <c.hartarski@gmail.com>
2025-08-18 10:58:34 +0300

    Fixed CompanyRepository to extend JpaSpecificationExecutor<Company>

--- src/main/java/com/butikimoti/real_estate_planner/repository/CompanyRepository.java
+++ src/main/java/com/butikimoti/real_estate_planner/repository/CompanyRepository.java
@@ -1,17 +1,15 @@
 package com.butikimoti.real_estate_planner.repository;
 
 import com.butikimoti.real_estate_planner.model.entity.Company;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, UUID> {
+public interface CompanyRepository extends JpaRepository<Company, UUID>,
+        JpaSpecificationExecutor<Company> {
     boolean existsByName(String name);
     Company findByName(String name);
-    Page<Company> findAll(Specification<Company> specification, Pageable pageable);
 }
