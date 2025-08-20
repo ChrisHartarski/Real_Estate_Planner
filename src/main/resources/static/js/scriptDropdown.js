4fd17f90a643b65210e54a791fb1d15b08b1ed8b
ChrisHartarski <c.hartarski@gmail.com>
2025-08-20 12:05:34 +0300

    Fix scriptDropdown.js

--- src/main/resources/static/js/scriptDropdown.js
+++ src/main/resources/static/js/scriptDropdown.js
@@ -2,14 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const dropdownMenuEl = document.getElementById("dropdownMenu");
     const profileBtnEl = document.getElementById("profileBtn");
 
-    profileBtnEl.addEventListener("click", (e) => {
-        dropdownMenuEl.classList.toggle("hidden");
-        e.stopPropagation();
-    });
+    if (profileBtnEl && dropdownMenuEl) {
+        profileBtnEl.addEventListener("click", (e) => {
+            dropdownMenuEl.classList.toggle("hidden");
+            e.stopPropagation();
+        });
 
-    document.addEventListener("click", (e) => {
-        if (!dropdownMenuEl.contains(e.target)) {
-            dropdownMenuEl.classList.add("hidden");
-        }
-    });
+        document.addEventListener("click", (e) => {
+            if (!dropdownMenuEl.contains(e.target)) {
+                dropdownMenuEl.classList.add("hidden");
+            }
+        });
+    }
 });
